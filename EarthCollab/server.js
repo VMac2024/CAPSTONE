bafb197530556fb8f7137b41dc6b7e7b065acd7d
VMac2024 <v.mcinman@gmail.com>
2025-03-22 11:17:02 +1300

    update commit

---
+++ EarthCollab/server.js
@@ -0,0 +1,17 @@
+//One Earth Community Collab
+
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+let dbConnect = require("./dbConnect");
+
+let userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 8081;
+console.log("PORT from env:", process.env.PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port
+${PORT}.`);
+});
